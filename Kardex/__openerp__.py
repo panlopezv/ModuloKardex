87bbb8aa28188576d2be284a33238860566db2b1
Pablo López <panlopezv@outlook.com>
2016-04-09 11:32:25 -0600

    Creacion de modulo
    Clases de modulo de prueba.

---
+++ Kardex/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "Gestión de Personas",
+    "version" : "1.0",
+    "author" : "Héctor Costa Guzmán",
+    "category" : "Generic Modules/Others",
+    "website" : "http://www.hcosta.info",
+    "description": "Módulo de prueba para gestionar personas.",
+    "depends" : ["base"],
+    "update_xml" : ["personas_view.xml"],
+    "active": True,
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
